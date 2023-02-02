758c44383a0066123acd88d554b4b3d077df800c
Bonie <83107580+boniewijaya2021@users.noreply.github.com>
2023-02-02 17:39:17 +0700

    git add .
    first commit

---
+++ src/main/java/com/desktops/spring/service/AlertService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.desktops.spring.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author bonie
+ */
+@Service
+public class AlertService {
+    public String alert(){
+        return "ini java";
+    }
+}
